5c6ba8f7ac08447a8182293825e6d9dde63bb362
Joseph-rumery <144263711+Joseph-rumery@users.noreply.github.com>
2023-09-27 14:26:14 -0400

    added mystack

---
+++ src/myStack.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class myStack implements IStack{
+	
+	ArrayList array = new ArrayList<>();
+	
+	public void push(String c) {
+		
+		
+	}
+	
+	public String pop() {
+		return "";
+	}
+	
+	public String peek() {
+		return "";
+	}
+	
+	public boolean isEmpty() {
+		return true;
+	}
+
+}
