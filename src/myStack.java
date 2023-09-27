dc886865c459b6de0bccd59adfe3d2db891987a2
Joseph-rumery <144263711+Joseph-rumery@users.noreply.github.com>
2023-09-27 14:35:38 -0400

    Update myStack.java

--- src/myStack.java
+++ src/myStack.java
@@ -1,23 +1,46 @@
+
 import java.util.*;
+
+//Joseph Rumery 
 public class myStack implements IStack{
 	
-	ArrayList array = new ArrayList<>();
+	//ArrayList to hold all String objects
+	ArrayList<String> array = new ArrayList<String>();
 	
+	//adds new String to top of stack 
 	public void push(String c) {
 		
+		//adds string to front of arraylist
+		array.add(0,c);
+	}
+	
+	//removes top of stack
+	public String pop() {
+		
+		//holds string of removed item
+		String temp = array.get(0);
+		
+		//removes item
+		array.remove(0);
+		
+		//returns the string that was removed
+		return temp;
+		
 		
 	}
 	
-	public String pop() {
-		return "";
-	}
-	
+	//looks at top string of stack 
 	public String peek() {
-		return "";
+		
+		//returns the string in the first spot of the arraylist
+		return array.get(0);
 	}
 	
+	//returns if stack is empty or not 
 	public boolean isEmpty() {
-		return true;
+		
+		//checks if array is empty 
+		return array.isEmpty();
 	}
 
 }
