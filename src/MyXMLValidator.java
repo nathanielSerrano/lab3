4a87d7711b9d9fd308ffc44aae9a2b01bc9dc697
SuperCudder <144271447+SuperCudder@users.noreply.github.com>
2023-09-28 16:09:11 -0400

    XMLValidatorTester V1

--- src/MyXMLValidator.java
+++ src/MyXMLValidator.java
@@ -1,63 +1,66 @@
 /**
- *@author Vivek Reddy Bhimavarapu
+ * @author Vivek Reddy Bhimavarapu
  */
 //import java.util.Stack;
 
 // MyXMLValidator class implementing the IXMLValidator interface
 public class MyXMLValidator implements IXMLValidator {
-    
-    // Override the validateXML method from the IXMLValidator interface
-    @Override
-    public boolean validateXML(IStack stack, String inputXML) {
-        
-        // Create a stack to store opening tags
-        IStack tagStack = new myStack();
-        
-        // Convert the input XML string to a character array for traversal
-        char[] chars = inputXML.toCharArray();
 
-        // Iterate through each character in the input XML
-        for (int i = 0; i < chars.length; i++) {
-            char currentChar = chars[i];
+	// Override the validateXML method from the IXMLValidator interface
+	@Override
+	public boolean validateXML(IStack stack, String inputXML) {
 
-            // Check if the current character is the start of a tag
-            if (currentChar == '<') {
-                // Create a StringBuilder to build the tag
-                StringBuilder tagBuilder = new StringBuilder();
+		// Create a stack to store opening tags
+		IStack tagStack = new myStack();
 
-                // Continue reading characters until the closing '>' of the tag is found
-                while (i < chars.length && chars[i] != '>') {
-                    tagBuilder.append(chars[i]);
-                    i++;
-                }
+		// Convert the input XML string to a character array for traversal
+		char[] chars = inputXML.toCharArray();
 
-                // Check if the loop ended without finding the closing '>'
-                if (i == chars.length || chars[i] != '>') {
-                    // Missing closing '>' for the tag, so the XML is invalid
-                    return false;
-                }
+		// Iterate through each character in the input XML
+		for (int i = 0; i < chars.length; i++) {
+			char currentChar = chars[i];
 
-                // Extract the tag as a String
-                String tag = tagBuilder.toString();
+			// Check if the current character is the start of a tag
+			if (currentChar == '<') {
+				// Create a StringBuilder to build the tag
+				StringBuilder tagBuilder = new StringBuilder();
 
-                // Check if the tag is a closing tag (starts with "</")
-                if (tag.startsWith("</")) {
-                    // It's a closing tag, so extract the opening tag
-                    String openingTag = tag.substring(2);
+				// Continue reading characters until the closing '>' of the tag is found
+				while (i < chars.length && chars[i] != '>') {
+					tagBuilder.append(chars[i]);
+					i++;
+				}
+				
 
-                    // Check if the tag stack is empty or if the opening tag matches the top of the stack
-                    if (tagStack.isEmpty() || !tagStack.pop().equals(openingTag)) {
-                        // Mismatched opening and closing tags, so the XML is invalid
-                        return false;
-                    }
-                } else if (!tag.startsWith("<?") && !tag.endsWith("/>")) {
-                    // It's an opening tag (ignore processing instructions and self-closing tags)
-                    tagStack.push(tag);
-                }
-            }
-        }
+				// Check if the loop ended without finding the closing '>'
+				if (i == chars.length || chars[i] != '>') {
+					// Missing closing '>' for the tag, so the XML is invalid
+					return false;
+				}
 
-        // If the stack is empty, all opening tags were properly closed by closing tags
-        return tagStack.isEmpty();
-    }
+				// Extract the tag as a String
+				String tag = tagBuilder.toString();
+
+				// Check if the tag is a closing tag (starts with "</")
+				if (tag.startsWith("</")) {
+					// It's a closing tag, so extract the opening tag
+					String openingTag = tag.substring(2);
+
+					// Check if the tag stack is empty or if the opening tag matches the top of the
+					// stack
+					if (tagStack.isEmpty() || !tagStack.pop().equals(openingTag)) {
+						// Mismatched opening and closing tags, so the XML is invalid
+						return false;
+					}
+				} else if (!tag.startsWith("<?") && !tag.endsWith("/>")) {
+					// It's an opening tag (ignore processing instructions and self-closing tags)
+					tagStack.push(tag);
+				}
+			}
+		}
+		System.out.println(chars);
+
+		// If the stack is empty, all opening tags were properly closed by closing tags
+		return tagStack.isEmpty();
+	}
 }
--- src/XMLValidatorTester.java
+++ src/XMLValidatorTester.java
@@ -1,4 +1,37 @@
-
+/**
+ * XMLValidatorTester, class to check correctness of myIXMLValidator using 5 XML
+ * Strings, returns number of correct results
+ * 
+ * @author Jonah
+ */
 public class XMLValidatorTester {
+	/**
+	 * precision method checks correctness of myIXMLValidator checking
+	 * 
+	 * @author Jonah
+	 * @param check IXMLValidator to test strings validity
+	 * @return correctCount number of correct matching Strings
+	 */
+	public double precision(IXMLValidator check) {
+		double correctCount = 0.0;
+		// stack of expected XML strings to test
+		myStack stack = new myStack();
+		// pushing strings to stack
+		stack.push("/book");
+		stack.push("<title>The Great Gatsby</title>");
+		stack.push("book");
+		stack.push("book");
+		stack.push("bookstore");
+
+		// while loop checking if stack is not empty, if check's validateXML boolean
+		// with the stack input and input strings are true, add to correctCount
+		while (!stack.isEmpty()) {
+			if (check.validateXML(stack, stack.pop()) == true) {
+				correctCount++;
+			}
+		}
+		// return number of correct strings
+		return correctCount;
+	}
 
 }
